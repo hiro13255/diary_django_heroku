95141ae5d7e0228a61551145816dd857e90f371c
rain1275 <kirakira7573@gmail.com>
2020-12-04 22:51:11 +0900

    プロジェクトディレクトリ指定

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -14,6 +14,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 # ロギング
 LOGGING = {
     'version': 1,
