0a76e722f5c61d38a2834056e80b1859de5fe026
rain1275 <kirakira7573@gmail.com>
2020-12-05 11:41:56 +0900

    動作確認の為一旦ミドルウェア戻す

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -79,7 +79,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'private_diary.urls'
