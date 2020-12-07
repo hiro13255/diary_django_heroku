7addd7ee353b84c877031c2a130dbbf2be457744
rain1275 <kirakira7573@gmail.com>
2020-12-07 15:18:48 +0900

    エラー調査

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -15,16 +15,16 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#    os.path.join(BASE_DIR, 'media'),
+#)
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
