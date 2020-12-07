79f6347a72077b56911580b9e195662f314141b2
rain1275 <kirakira7573@gmail.com>
2020-12-07 15:33:19 +0900

    本番用設定ファイルに修正

--- private_diary/wsgi.py
+++ private_diary/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 #from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'private_diary.settings_dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'private_diary.settings')
 
 application = get_wsgi_application()
 #application = DjangoWhiteNoise(application)
